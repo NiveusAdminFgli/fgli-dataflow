e9e5d84dd90b414c863dc31b1bc6356ed434aeb0
chaithra <chaithra.k@niveussolutions.com>
2023-01-11 17:23:22 +0530

    first commit

--- src/main/java/org/example/MyOptions.java
+++ src/main/java/org/example/MyOptions.java
@@ -1,11 +1,11 @@
-package org.example;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.ValueProvider;
+// package org.example;
+// import org.apache.beam.sdk.options.PipelineOptions;
+// import org.apache.beam.sdk.options.ValueProvider;
 
-public interface MyOptions extends PipelineOptions{
+// public interface MyOptions extends PipelineOptions{
 
-    ValueProvider<String> getUsername();
-    void setUsername(ValueProvider<String> value);
-    ValueProvider<String> getPassword();
-	void setPassword(ValueProvider<String> value);
-}
+//     ValueProvider<String> getUsername();
+//     void setUsername(ValueProvider<String> value);
+//     ValueProvider<String> getPassword();
+// 	void setPassword(ValueProvider<String> value);
+// }
